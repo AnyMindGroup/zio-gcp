a915f1ebd4a8bcbfe6e636dc3537642627a625a6
Roman Langolf <rolang@pm.me>
2024-11-03 23:15:31 +0700

    addauthed backend constructor

--- zio-gc-auth/shared/src/main/scala/com/anymindgroup/gcp/auth/AuthedBackend.scala
+++ zio-gc-auth/shared/src/main/scala/com/anymindgroup/gcp/auth/AuthedBackend.scala
@@ -1,18 +1,24 @@
 package com.anymindgroup.gcp.auth
 
 import sttp.capabilities.Effect
-import sttp.client4.{GenericBackend, GenericRequest, Response}
+import sttp.client4.{Backend, GenericBackend, GenericRequest, Response}
 import sttp.monad.MonadError
 
 import zio.Task
 
 object AuthedBackend {
-  def apply[P](tp: TokenProvider[Token], backend: GenericBackend[Task, P]) = new GenericBackend[Task, P] {
-    override def send[T](request: GenericRequest[T, P & Effect[Task]]): Task[Response[T]] =
+  def apply[P](tp: TokenProvider[Token], backend: GenericBackend[Task, P]): GenericBackend[Task, P] =
+    new GenericBackend[Task, P] {
+      override def send[T](request: GenericRequest[T, P & Effect[Task]]): Task[Response[T]] =
+        tp.token.flatMap(t => backend.send(request.auth.bearer(t.token.token)))
+      override def close(): Task[Unit]     = backend.close()
+      override def monad: MonadError[Task] = backend.monad
+    }
+
+  def apply(tp: TokenProvider[Token], backend: Backend[Task]): Backend[Task] = new Backend[Task] {
+    override def send[T](request: GenericRequest[T, Any & Effect[Task]]): Task[Response[T]] =
       tp.token.flatMap(t => backend.send(request.auth.bearer(t.token.token)))
-
-    override def close(): Task[Unit] = backend.close()
-
+    override def close(): Task[Unit]     = backend.close()
     override def monad: MonadError[Task] = backend.monad
   }
 }
