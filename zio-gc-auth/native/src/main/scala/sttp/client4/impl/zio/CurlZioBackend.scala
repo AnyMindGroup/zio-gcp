c7414958102b17e1a45506a44a3aaf87de8ee9cb
Roman Langolf <rolang@pm.me>
2024-11-10 12:39:49 +0700

    refactor curl zio backend

--- zio-gc-auth/native/src/main/scala/sttp/client4/impl/zio/CurlZioBackend.scala
+++ zio-gc-auth/native/src/main/scala/sttp/client4/impl/zio/CurlZioBackend.scala
@@ -2,20 +2,12 @@ package sttp.client4.impl.zio
 
 import _root_.zio.*
 import sttp.client4.*
-import sttp.client4.curl.AbstractCurlBackend
-import sttp.client4.curl.internal.CurlApi.*
-import sttp.client4.curl.internal.CurlCode.CurlCode
-import sttp.client4.wrappers.FollowRedirectsBackend
+import sttp.client4.curl.AbstractSyncCurlBackend
 
 class CurlZioBackend private (verbose: Boolean)
-    extends AbstractCurlBackend[Task](new RIOMonadAsyncError, verbose)
-    with Backend[Task] {
-
-  override def performCurl(c: CurlHandle): Task[CurlCode] = ZIO.attemptBlocking(c.perform)
-
-}
+    extends AbstractSyncCurlBackend[Task](new RIOMonadAsyncError, verbose)
+    with Backend[Task]
 
 object CurlZioBackend {
-  def apply(verbose: Boolean = false): Backend[Task] =
-    FollowRedirectsBackend(new CurlZioBackend(verbose))
+  def apply(verbose: Boolean = false): Backend[Task] = new CurlZioBackend(verbose)
 }
