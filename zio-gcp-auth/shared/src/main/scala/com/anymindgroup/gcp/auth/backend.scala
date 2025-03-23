a2b8ceb0f554904465f1edc780d34a83ab7ccdce
Roman Langolf <rolang@pm.me>
2025-03-23 14:24:07 +0700

    add AuthedBackend companion obj

--- zio-gcp-auth/shared/src/main/scala/com/anymindgroup/gcp/auth/backend.scala
+++ zio-gcp-auth/shared/src/main/scala/com/anymindgroup/gcp/auth/backend.scala
@@ -9,41 +9,46 @@ import zio.*
 
 trait AuthedBackend extends Backend[Task]
 
-def toAuthedBackend(tp: TokenProvider[Token], backend: Backend[Task]): AuthedBackend =
-  new AuthedBackend:
-    override def send[T](request: GenericRequest[T, Effect[Task]]): Task[Response[T]] =
-      tp.token.flatMap(t => backend.send(request.auth.bearer(t.token.token)))
-    override def close(): Task[Unit]     = backend.close()
-    override def monad: MonadError[Task] = backend.monad
+object AuthedBackend:
+  def apply(tp: TokenProvider[Token], backend: Backend[Task]): AuthedBackend =
+    new AuthedBackend:
+      override def send[T](request: GenericRequest[T, Effect[Task]]): Task[Response[T]] =
+        tp.token.flatMap(t => backend.send(request.auth.bearer(t.token.token)))
+      override def close(): Task[Unit]     = backend.close()
+      override def monad: MonadError[Task] = backend.monad
 
-def defaultAccessTokenBackend(
-  lookupComputeMetadataFirst: Boolean = false,
-  refreshRetrySchedule: Schedule[Any, Any, Any] = TokenProvider.defaults.refreshRetrySchedule,
-  refreshAtExpirationPercent: Double = TokenProvider.defaults.refreshAtExpirationPercent,
-): ZIO[Scope, Throwable, AuthedBackend] =
-  httpBackendScoped().flatMap: backend =>
-    TokenProvider
-      .defaultAccessTokenProvider(
-        backend = backend,
-        lookupComputeMetadataFirst = lookupComputeMetadataFirst,
-        refreshRetrySchedule = refreshRetrySchedule,
-        refreshAtExpirationPercent = refreshAtExpirationPercent,
-      )
-      .map(tp => toAuthedBackend(tp, backend))
+  def defaultAccessTokenBackend(
+    lookupComputeMetadataFirst: Boolean = false,
+    refreshRetrySchedule: Schedule[Any, Any, Any] = TokenProvider.defaults.refreshRetrySchedule,
+    refreshAtExpirationPercent: Double = TokenProvider.defaults.refreshAtExpirationPercent,
+  ): ZIO[Scope, Throwable, AuthedBackend] =
+    httpBackendScoped().flatMap: backend =>
+      TokenProvider
+        .defaultAccessTokenProvider(
+          backend = backend,
+          lookupComputeMetadataFirst = lookupComputeMetadataFirst,
+          refreshRetrySchedule = refreshRetrySchedule,
+          refreshAtExpirationPercent = refreshAtExpirationPercent,
+        )
+        .map(tp => AuthedBackend(tp, backend))
 
-def defaultIdTokenBackend(
-  audience: String,
-  lookupComputeMetadataFirst: Boolean = false,
-  refreshRetrySchedule: Schedule[Any, Any, Any] = TokenProvider.defaults.refreshRetrySchedule,
-  refreshAtExpirationPercent: Double = TokenProvider.defaults.refreshAtExpirationPercent,
-): ZIO[Scope, Throwable, AuthedBackend] =
-  httpBackendScoped().flatMap: backend =>
-    TokenProvider
-      .defaultIdTokenProvider(
-        audience = audience,
-        backend = backend,
-        lookupComputeMetadataFirst = lookupComputeMetadataFirst,
-        refreshRetrySchedule = refreshRetrySchedule,
-        refreshAtExpirationPercent = refreshAtExpirationPercent,
-      )
-      .map(tp => toAuthedBackend(tp, backend))
+  def defaultIdTokenBackend(
+    audience: String,
+    lookupComputeMetadataFirst: Boolean = false,
+    refreshRetrySchedule: Schedule[Any, Any, Any] = TokenProvider.defaults.refreshRetrySchedule,
+    refreshAtExpirationPercent: Double = TokenProvider.defaults.refreshAtExpirationPercent,
+  ): ZIO[Scope, Throwable, AuthedBackend] =
+    httpBackendScoped().flatMap: backend =>
+      TokenProvider
+        .defaultIdTokenProvider(
+          audience = audience,
+          backend = backend,
+          lookupComputeMetadataFirst = lookupComputeMetadataFirst,
+          refreshRetrySchedule = refreshRetrySchedule,
+          refreshAtExpirationPercent = refreshAtExpirationPercent,
+        )
+        .map(tp => AuthedBackend(tp, backend))
+
+export AuthedBackend.apply as toAuthedBackend
+export AuthedBackend.defaultAccessTokenBackend
+export AuthedBackend.defaultIdTokenBackend
