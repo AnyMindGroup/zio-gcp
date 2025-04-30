4271e23e5924056762ba20c777c1ae68706cf912
Roman Langolf <rolang@pm.me>
2025-04-30 15:20:15 +0700

    add tests, update docs

---
+++ tests/shared/src/test/scala/StorageSpec.scala
@@ -0,0 +1,66 @@
+package com.anymindgroup.gcp.storage
+package v1
+
+import java.nio.charset.StandardCharsets
+
+import com.anymindgroup.gcp.auth.defaultAccessTokenBackend
+import sttp.client4.{Response, quick}
+import sttp.model.{Header, MediaType, Method}
+
+import zio.*
+import zio.Console.{printError, printLine}
+import zio.test.*
+
+// test for manual execution
+object StorageSpec extends ZIOSpecDefault:
+  def spec = suite("StorageSpec")(
+    test("insert object, create signed url, delete object") {
+      for {
+        (bucket, svcAcc) <- ZIO.fromEither:
+                              for
+                                b <- sys.env.get("GCP_TEST_STORAGE_BUCKET").toRight("Missing GCP_TEST_STORAGE_BUCKET")
+                                s <- sys.env.get("GCP_TEST_SERVICE_ACCOUNT").toRight("Missing GCP_TEST_SERVICE_ACCOUNT")
+                              yield (b, s)
+        backend <- defaultAccessTokenBackend()
+        objPath  = List("manual_test", "zio_gcp_test.txt")
+        body     = "this is a test body".getBytes(StandardCharsets.UTF_8)
+
+        // insert object
+        _ <- resources.Objects
+               .insert(bucket = bucket, name = Some(objPath.mkString("/")))
+               .headers(Header.contentType(MediaType.TextPlain), Header.contentLength(body.length))
+               .body(body)
+               .send(backend)
+               .flatMap:
+                 _.body match
+                   case Left(err)   => ZIO.dieMessage(s"❌ Failure on creating object: $err")
+                   case Right(body) => printLine(s"✅ Object created with id: ${body.id.getOrElse("")}")
+
+        // create signed url
+        signedUrl <- V4SignUrlRequestBuilder
+                       .create()
+                       .signUrlRequest(
+                         bucket = bucket,
+                         resourcePath = objPath,
+                         contentType = None,
+                         method = Method.GET,
+                         serviceAccountEmail = svcAcc,
+                         signAlgorithm = V4SignAlgorithm.`GOOG4-RSA-SHA256`,
+                         expiresInSeconds = V4SignatureExpiration.inSeconds(300),
+                       )
+                       .flatMap(_.send(backend).flatMap(r => ZIO.fromEither(r.body)))
+        _ <- printLine(s"✅ Created signed url: $signedUrl")
+
+        // test signed url
+        _ <- quick.backend.send(quick.basicRequest.get(signedUrl)) match
+               case res if res.code.isSuccess => printLine("✅ Signed url ok")
+               case res                       => printError(s"❌ Signed url failure: ${res.code.code} / ${res.body}")
+
+        // delete object
+        _ <- backend.send(resources.Objects.delete(`object` = objPath.mkString("/"), bucket = bucket)).flatMap {
+               case Response(Right(body), _, _, _, _, _) => printLine(s"✅ Object deleted: $body")
+               case Response(Left(err), _, _, _, _, _)   => printError(s"❌ Failure on deleting object: $err")
+             }
+      } yield assertCompletes
+    }
+  ) @@ TestAspect.withLiveSystem @@ TestAspect.withLiveClock @@ TestAspect.ifEnvNotSet("CI")
--- tests/shared/src/test/scala/V4CanonicalRequestBuilderSpec.scala
+++ tests/shared/src/test/scala/V4CanonicalRequestBuilderSpec.scala
@@ -2,14 +2,14 @@ package com.anymindgroup.gcp.storage
 
 import java.time.Instant
 
-import sttp.model.Method
+import sttp.model.{MediaType, Method}
 
 import zio.ZIO
 import zio.test.{ZIOSpecDefault, *}
 
 object V4CanonicalRequestBuilderSpec extends ZIOSpecDefault:
-  override def spec: Spec[Any, Any] = suite("SigningServiceSpec")(
-    test("toCanonicalRequest") {
+  override def spec: Spec[Any, Any] = suite("V4CanonicalRequestBuilderSpec")(
+    test("toCanonicalRequest (PUT, no content type)") {
       val testTimestamp  = Instant.parse("2019-12-01T19:08:59Z")
       val testSvcAccount = "test@gcp.iam.gserviceaccount.com"
 
@@ -19,7 +19,7 @@ object V4CanonicalRequestBuilderSpec extends ZIOSpecDefault:
                  builder.toCanonicalRequest(
                    method = Method.PUT,
                    timestamp = testTimestamp,
-                   resourcePath = "test/image.png",
+                   resourcePath = List("test", "image.png"),
                    contentType = None,
                    bucket = "test-bucket",
                    serviceAccountEmail = testSvcAccount,
@@ -45,5 +45,43 @@ object V4CanonicalRequestBuilderSpec extends ZIOSpecDefault:
                                       |d62c091b215aaae1a3ae90601dfae7623839ff5aab8980860bcea13748895465""".stripMargin
              )
       } yield assertCompletes
-    }
+    },
+    test("toCanonicalRequest (GET, with content type)") {
+      val testTimestamp  = Instant.parse("2019-12-01T19:08:59Z")
+      val testSvcAccount = "test@gcp.iam.gserviceaccount.com"
+
+      for {
+        builder <- ZIO.succeed(V4CanonicalRequestBuilder())
+        req <- ZIO.fromEither(
+                 builder.toCanonicalRequest(
+                   method = Method.GET,
+                   timestamp = testTimestamp,
+                   resourcePath = List("test", "image.png"),
+                   contentType = Some(MediaType.ImagePng),
+                   bucket = "test-bucket",
+                   serviceAccountEmail = testSvcAccount,
+                   signAlgorithm = V4SignAlgorithm.`GOOG4-RSA-SHA256`,
+                   expiresInSeconds = V4SignatureExpiration.inSeconds(900),
+                 )
+               )
+        _ <-
+          assertTrue(
+            req.payloadPlain ==
+              """|GET
+                 |/test-bucket/test/image.png
+                 |X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=test%40gcp.iam.gserviceaccount.com%2F20191201%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20191201T190859Z&X-Goog-Expires=900&X-Goog-SignedHeaders=content-type%3Bhost
+                 |content-type:image/png
+                 |host:storage.googleapis.com
+                 |
+                 |content-type;host
+                 |UNSIGNED-PAYLOAD""".stripMargin
+          )
+        _ <- assertTrue(
+               req.stringToSign == """|GOOG4-RSA-SHA256
+                                      |20191201T190859Z
+                                      |20191201/auto/storage/goog4_request
+                                      |012eb316298a5202f1b404bb5d0be0c6c270d1442eda75b055303df96b05c137""".stripMargin
+             )
+      } yield assertCompletes
+    },
   )
