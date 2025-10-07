380ad93c4934a3818a979cc017f4c1b12055f2e4
Scala Steward <scala_steward@virtuslab.com>
2025-10-07 13:16:26 +0000

    Update zio-gcp-aiplatform-v1, ... to 0.2.4

--- examples/shared/src/main/scala/storage_example.scala
+++ examples/shared/src/main/scala/storage_example.scala
@@ -1,6 +1,6 @@
 //> using scala 3.7.3
-//> using dep com.anymindgroup::zio-gcp-auth::0.2.3
-//> using dep com.anymindgroup::zio-gcp-storage::0.2.3
+//> using dep com.anymindgroup::zio-gcp-auth::0.2.4
+//> using dep com.anymindgroup::zio-gcp-storage::0.2.4
 
 import zio.*, com.anymindgroup.gcp.*, storage.*, auth.defaultAccessTokenBackend
 import v1.resources.Objects, sttp.model.{Header, MediaType, Method}
