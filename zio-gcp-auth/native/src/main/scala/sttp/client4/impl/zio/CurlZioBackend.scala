86b6633c1831d27129049bcc88e496ab2416708d
Roman Langolf <rolang@pm.me>
2026-03-07 13:27:51 +0700

    remove custom curl backend

--- zio-gcp-auth/native/src/main/scala/sttp/client4/impl/zio/CurlZioBackend.scala
+++ zio-gcp-auth/native/src/main/scala/sttp/client4/impl/zio/CurlZioBackend.scala
@@ -3,7 +3,9 @@ package sttp.client4.impl.zio
 import _root_.zio.*
 import sttp.client4.*
 
-class CurlZioBackend private (verbose: Boolean) extends sttp.client4.curl.TaskCurlBackend(verbose) with Backend[Task]
+class CurlZioBackend private (verbose: Boolean)
+    extends sttp.client4.curl.AbstractSyncCurlBackend[Task](RIOMonadAsyncError[Any], verbose)
+    with Backend[Task]
 
 object CurlZioBackend {
   def apply(verbose: Boolean = false): Backend[Task] = new CurlZioBackend(verbose)
