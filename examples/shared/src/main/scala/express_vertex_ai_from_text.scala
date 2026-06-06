be4a3d83f04ca33c5fb581b454a8a86089234180
Roman Langolf <rolang@pm.me>
2026-06-06 18:44:30 +0700

    update examples

--- examples/shared/src/main/scala/express_vertex_ai_from_text.scala
+++ examples/shared/src/main/scala/express_vertex_ai_from_text.scala
@@ -1,26 +1,24 @@
-import zio.*, zio.schema.*, com.anymindgroup.gcp.*, auth.defaultAccessTokenBackend
-import com.anymindgroup.gcp.aiplatform.*
-import com.anymindgroup.gcp.aiplatform.v1.*, v1.resources.*, v1.schemas.*
-import com.github.plokhotnyuk.jsoniter_scala.core.*
-import com.github.plokhotnyuk.jsoniter_scala.macros.*
+import zio.*, zio.schema.*, com.anymindgroup.gcp.auth.defaultAccessTokenBackend
+import com.anymindgroup.gcp.aiplatform.Express
+import com.github.plokhotnyuk.jsoniter_scala as json
 
 object express_vertex_ai_from_text extends ZIOAppDefault:
   def run = for
     authedBackend <- defaultAccessTokenBackend()
     express        = Express(
-                       backend = authedBackend,
-                       defaultProjectsId = "my-gcp-project",
-                       defaultLocationsId = Endpoint.`asia-northeast1`.location,
-                       defaultPublishersId = "google",
-                       defaultModelsId = "gemini-3.5-flash",
-                     )
+                backend = authedBackend,
+                defaultProjectsId = "my-gcp-project",
+                defaultLocationsId = "global",
+                defaultPublishersId = "google",
+                defaultModelsId = "gemini-3.5-flash",
+              )
 
-    txt  <- express.generateTextFromText("hello how are you doing?")
-    _    <- ZIO.logInfo("Text response: " + txt)
+    txt <- express.generateTextFromText("hello how are you doing?")
+    _   <- ZIO.logInfo("Text response: " + txt)
 
     joke <- express.generateStructuredFromText[Joke]("Tell me a joke")
     _    <- ZIO.logInfo("Joke: " + joke.setup + " \u2014 " + joke.punchline)
   yield ()
 
   case class Joke(setup: String, punchline: String) derives Schema
-  given JsonValueCodec[Joke] = JsonCodecMaker.make
+  given json.core.JsonValueCodec[Joke] = json.macros.JsonCodecMaker.make
